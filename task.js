adb59e0d9a84ece38a995b45c58ab51f3de43576
Semyon Zarajsky <zarajskysam92@gmail.com>
2020-07-27 16:31:38 +0300

    commit added

---
+++ task.js
@@ -0,0 +1,23 @@
+let modal1 = document.getElementById('modal_main');
+let modal2 = document.getElementById('modal_success');
+let close = document.getElementsByClassName('modal__close');
+
+
+
+console.log(close);
+modal1.classList.add('modal_active');
+
+for (let i = 0; i < close.length; i++ ) {
+    close[i].onclick = function() {
+        modal1.classList.remove('modal_active');
+        modal2.classList.remove('modal_active');
+    }
+}
+
+
+let success = close.item(1);
+success.onclick = function() {
+    modal1.classList.remove('modal_active');
+    modal2.classList.add('modal_active');
+}
+console.log(success);
